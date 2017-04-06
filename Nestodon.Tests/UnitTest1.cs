6a7f21971366848e99c62b8e3e117b15d231d28d
Guillaume Lacasa <github@adhess.net>
2017-04-06 20:04:36 +0200

    Working on app creation and login

--- Nestodon.Tests/UnitTest1.cs
+++ Nestodon.Tests/UnitTest1.cs
@@ -6,14 +6,20 @@ namespace Nestodon.Tests
 {
     public class UnitTest1
     {
+
         [Fact]
         public async Task GetAccount()
         {
-            var client = new NestodonClient("mastodon.cx");
-            var account = await client.GetAccount("glacasa");
+            var client = new NestodonClient("mamot.fr", "ecb051ae66b6989112afc496153f6141bd0bd00398c3c189aaad406f69b93a8b", "a38eb97955d84e6b5fde759dc2c6a9d277783c4774686846f8c07a664102b528");
 
-            Assert.Equal("glacasa", account.UserName);
-            Assert.Equal("Guillaume Lacasa", account.DisplayName);
+            //await client.RegisterApp("NestodonTest");
+            //await client.Connect("mastodon@adhess.net", "");
+
+            //var account = await client.GetAccount("glacasa");
+
+
+            //Assert.Equal("gltesting", account.UserName);
+            //Assert.Equal("", account.DisplayName);
         }
     }
 }
--- Nestodon/NestodonClient.cs
+++ Nestodon/NestodonClient.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Nestodon.Entities;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Nestodon
 {
@@ -10,16 +12,35 @@ namespace Nestodon
     {
         public string Instance { get; }
 
-        /// <summary>
-        /// Create a new client
-        /// </summary>
+        public string ClientId { get;  }
+
+        public string ClientSecret { get;  }
+
+        #region Ctor
+
         public NestodonClient(string instance)
         {
             this.Instance = instance;
         }
 
+        public NestodonClient(string instance, string clientId, string clientSecret)
+        {
+            this.Instance = instance;
+            this.ClientId = clientId;
+            this.ClientSecret = clientSecret;
+        }
 
-        private async Task<string> SendRequest(string route, string method = "GET")
+        #endregion
+
+        #region Http helpers
+
+        private static void AddHttpHeader(HttpClient client)
+        {
+            // TODO : add access token
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer ");
+        }
+
+        private async Task<string> Get(string route)
         {
             string url = "https://" + this.Instance + route;
 
@@ -28,10 +49,45 @@ namespace Nestodon
             return await response.Content.ReadAsStringAsync();
         }
 
-        private async Task<T> GetObject<T>(string route, string method = "GET")
+        private async Task<T> GetObject<T>(string route)
         {
-            var content = await SendRequest(route, method);
+            var content = await Get(route);
             return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        private async Task<string> Post(string route, IEnumerable<KeyValuePair<string, string>> data)
+        {
+            string url = "https://" + this.Instance + route;
+
+            var client = new HttpClient();
+            var content = new FormUrlEncodedContent(data);
+            var response = await client.PostAsync(url, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        #endregion
+
+        public async Task Connect(string email, string password)
+        {
+            var data = new List<KeyValuePair<string, string>>();
+            data.Add(new KeyValuePair<string, string>("client_id", ClientId));
+            data.Add(new KeyValuePair<string, string>("client_secret", ClientSecret));
+            data.Add(new KeyValuePair<string, string>("grant_type", "password"));
+            data.Add(new KeyValuePair<string, string>("username", email));
+            data.Add(new KeyValuePair<string, string>("password", password));
+
+            var response = await Post("/oauth/token", data);
+
+        }
+
+        public async Task RegisterApp(string appName)
+        {
+            var data = new List<KeyValuePair<string, string>>();
+            data.Add(new KeyValuePair<string, string>("client_name", appName));
+            data.Add(new KeyValuePair<string, string>("redirect_uris", "urn:ietf:wg:oauth:2.0:oob"));
+            data.Add(new KeyValuePair<string, string>("scopes", "read"));
+
+            var response = await Post("/api/v1/apps", data);
         }
 
 
