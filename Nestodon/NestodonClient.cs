9a273bd089980bd14a1aedff684da9438639a16e
Guillaume Lacasa <github@adhess.net>
2017-04-07 13:39:39 +0200

    Just a little syntax update

--- Nestodon/NestodonClient.cs
+++ Nestodon/NestodonClient.cs
@@ -75,12 +75,16 @@ namespace Nestodon
 
         public async Task<Auth> Connect(string email, string password)
         {
-            var data = new List<KeyValuePair<string, string>>();
-            data.Add(new KeyValuePair<string, string>("client_id", AppRegistration.ClientId));
-            data.Add(new KeyValuePair<string, string>("client_secret", AppRegistration.ClientSecret));
-            data.Add(new KeyValuePair<string, string>("grant_type", "password"));
-            data.Add(new KeyValuePair<string, string>("username", email));
-            data.Add(new KeyValuePair<string, string>("password", password));
+            var data = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("client_id", AppRegistration.ClientId),
+                new KeyValuePair<string, string>("client_secret", AppRegistration.ClientSecret),
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", email),
+                new KeyValuePair<string, string>("password", password),
+            };
+
+
 
             this.UserAuth = await Post<Auth>("/oauth/token", data);
             return this.UserAuth;
@@ -88,10 +92,11 @@ namespace Nestodon
 
         public async Task<AppRegistration> RegisterApp(string appName)
         {
-            var data = new List<KeyValuePair<string, string>>();
-            data.Add(new KeyValuePair<string, string>("client_name", appName));
-            data.Add(new KeyValuePair<string, string>("redirect_uris", "urn:ietf:wg:oauth:2.0:oob"));
-            data.Add(new KeyValuePair<string, string>("scopes", "read"));
+            var data = new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("client_name", appName),
+                new KeyValuePair<string, string>("redirect_uris", "urn:ietf:wg:oauth:2.0:oob"),
+                new KeyValuePair<string, string>("scopes", "read"),
+            };
 
             this.AppRegistration = await Post<AppRegistration>("/api/v1/apps", data);
 
