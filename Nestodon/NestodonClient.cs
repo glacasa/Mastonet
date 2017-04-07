c185e1d7e350b7e1816aec5d5ef6eab9200cfaed
Guillaume Lacasa <github@adhess.net>
2017-04-07 19:06:59 +0200

    API methods for accounts (not implemented)

--- Nestodon/NestodonClient.cs
+++ Nestodon/NestodonClient.cs
@@ -73,18 +73,6 @@ namespace Nestodon
 
         #endregion
 
-        public async Task<AppRegistration> RegisterApp(string appName)
-        {
-            var data = new List<KeyValuePair<string, string>>() {
-                new KeyValuePair<string, string>("client_name", appName),
-                new KeyValuePair<string, string>("redirect_uris", "urn:ietf:wg:oauth:2.0:oob"),
-                new KeyValuePair<string, string>("scopes", "read"),
-            };
-
-            this.AppRegistration = await Post<AppRegistration>("/api/v1/apps", data);
-
-            return this.AppRegistration;
-        }
 
         public async Task<Auth> Connect(string email, string password)
         {
@@ -104,26 +92,54 @@ namespace Nestodon
         }
 
 
+        #region Accounts
+
+        /// <summary>
+        /// Fetching an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns an Account</returns>
         public Task<Account> GetAccount(int accountId)
         {
             return GetObject<Account>($"/api/v1/accounts/{accountId}");
         }
 
+        /// <summary>
+        /// Getting the current user
+        /// </summary>
+        /// <returns>Returns the authenticated user's Account</returns>
         public Task<Account> GetCurrentUser()
         {
             return GetObject<Account>($"/api/v1/accounts/verify_credentials");
         }
 
-        public Task<IEnumerable<Account>> GetFollowers(int accountId)
+        /// <summary>
+        /// Getting an account's followers
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns an array of Accounts</returns>
+        public Task<IEnumerable<Account>> GetAccountFollowers(int accountId)
         {
             return GetObject<IEnumerable<Account>>($"/api/v1/accounts/{accountId}/followers");
         }
 
-        public Task<IEnumerable<Account>> GetFollowing(int accountId)
+        /// <summary>
+        /// Getting who account is following
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns an array of Accounts</returns>
+        public Task<IEnumerable<Account>> GetAccountFollowing(int accountId)
         {
             return GetObject<IEnumerable<Account>>($"/api/v1/accounts/{accountId}/following");
         }
 
+        /// <summary>
+        /// Getting an account's statuses
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <param name="onlyMedia">Only return statuses that have media attachments</param>
+        /// <param name="excludeReplies">Skip statuses that reply to other statuses</param>
+        /// <returns>Returns an array of Statuses</returns>
         public Task<IEnumerable<Status>> GetAccountStatuses(int accountId, bool onlyMedia = false, bool excludeReplies = false)
         {
             var url = $"/api/v1/accounts/{accountId}/statuses";
@@ -149,5 +165,114 @@ namespace Nestodon
             return GetObject<IEnumerable<Status>>(url + queryParams);
         }
 
+        /// <summary>
+        /// Following an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns the target Account</returns>
+        public Task<Account> Follow(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Unfollowing an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns the target Account</returns>
+        public Task<Account> Unfollow(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Blocking an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns the target Account</returns>
+        public Task<Account> Block(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Unblocking an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns the target Account</returns>
+        public Task<Account> Unblock(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Muting an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns the target Account</returns>
+        public Task<Account> Mute(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Unmuting an account
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns>Returns the target Account</returns>
+        public Task<Account> Unmute(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Getting an account's relationships
+        /// </summary>
+        /// <param name="id">Account ID</param>
+        /// <returns>Returns an array of Relationships of the current user to a given account</returns>
+        public Task<IEnumerable<Relationship>> GetAccountRelationships(int id)
+        {
+            return GetAccountRelationships(new int[] { id });
+        }
+
+        /// <summary>
+        /// Getting an account's relationships
+        /// </summary>
+        /// <param name="id">Account IDs</param>
+        /// <returns>Returns an array of Relationships of the current user to a list of given accounts</returns>
+        public Task<IEnumerable<Relationship>> GetAccountRelationships(IEnumerable<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Searching for accounts
+        /// </summary>
+        /// <param name="q">What to search for</param>
+        /// <param name="limit">Maximum number of matching accounts to return (default: 40)</param>
+        /// <returns>Returns an array of matching Accounts. Will lookup an account remotely if the search term is in the username@domain format and not yet in the database</returns>
+        public Task<IEnumerable<Account>> SearchAccounts(string q, int? limit = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Apps
+
+        public async Task<AppRegistration> RegisterApp(string appName)
+        {
+            var data = new List<KeyValuePair<string, string>>() {
+                new KeyValuePair<string, string>("client_name", appName),
+                new KeyValuePair<string, string>("redirect_uris", "urn:ietf:wg:oauth:2.0:oob"),
+                new KeyValuePair<string, string>("scopes", "read"),
+            };
+
+            this.AppRegistration = await Post<AppRegistration>("/api/v1/apps", data);
+
+            return this.AppRegistration;
+        }
+
+        #endregion
     }
 }
