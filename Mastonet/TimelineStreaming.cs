335f3f032c48f365df9dcb7a6787339f831e5cf4
KMY <tt@kmycode.net>
2017-04-23 10:33:19 +0900

    There are no need to handle any error in Start method befause method become waitable.

--- Mastonet/TimelineStreaming.cs
+++ Mastonet/TimelineStreaming.cs
@@ -19,7 +19,6 @@ namespace Mastonet
         public event EventHandler<StreamUpdateEventArgs> OnUpdate;
         public event EventHandler<StreamNotificationEventArgs> OnNotification;
         public event EventHandler<StreamDeleteEventArgs> OnDelete;
-        public event EventHandler<StreamErrorEventArgs> OnError;
 
         internal TimelineStreaming(string url, string accessToken)
         {
@@ -30,58 +29,51 @@ namespace Mastonet
 
         public async Task Start()
         {
-            try
-            {
-                client = new HttpClient();
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
 
-                var stream = await client.GetStreamAsync(url);
+            var stream = await client.GetStreamAsync(url);
 
-                var reader = new StreamReader(stream);
+            var reader = new StreamReader(stream);
                 
-                string eventName = null;
-                string data = null;
+            string eventName = null;
+            string data = null;
 
-                while (client != null)
+            while (client != null)
+            {
+                var line = await reader.ReadLineAsync();
+
+
+                if (string.IsNullOrEmpty(line) || line.StartsWith(":"))
                 {
-                    var line = await reader.ReadLineAsync();
+                    eventName = data = null;
+                    continue;
+                }
 
+                if (line.StartsWith("event: "))
+                {
+                    eventName = line.Substring("event: ".Length).Trim();
+                }
+                else if (line.StartsWith("data: "))
+                {
+                    data = line.Substring("data: ".Length);
 
-                    if (string.IsNullOrEmpty(line) || line.StartsWith(":"))
+                    switch (eventName)
                     {
-                        eventName = data = null;
-                        continue;
-                    }
-
-                    if (line.StartsWith("event: "))
-                    {
-                        eventName = line.Substring("event: ".Length).Trim();
-                    }
-                    else if (line.StartsWith("data: "))
-                    {
-                        data = line.Substring("data: ".Length);
-
-                        switch (eventName)
-                        {
-                            case "update":
-                                var status = JsonConvert.DeserializeObject<Status>(data);
-                                OnUpdate?.Invoke(this, new StreamUpdateEventArgs() { Status = status });
-                                break;
-                            case "notification":
-                                var notification = JsonConvert.DeserializeObject<Notification>(data);
-                                OnNotification?.Invoke(this, new StreamNotificationEventArgs() { Notification = notification });
-                                break;
-                            case "delete":
-                                var statusId = int.Parse(data);
-                                OnDelete?.Invoke(this, new StreamDeleteEventArgs() { StatusId = statusId });
-                                break;
-                        }
+                        case "update":
+                            var status = JsonConvert.DeserializeObject<Status>(data);
+                            OnUpdate?.Invoke(this, new StreamUpdateEventArgs() { Status = status });
+                            break;
+                        case "notification":
+                            var notification = JsonConvert.DeserializeObject<Notification>(data);
+                            OnNotification?.Invoke(this, new StreamNotificationEventArgs() { Notification = notification });
+                            break;
+                        case "delete":
+                            var statusId = int.Parse(data);
+                            OnDelete?.Invoke(this, new StreamDeleteEventArgs() { StatusId = statusId });
+                            break;
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                OnError?.Invoke(this, new StreamErrorEventArgs() { Exception = ex });
             }
             this.Stop();
         }
