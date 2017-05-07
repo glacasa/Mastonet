b5fbf6828e36245a1766984198abc9d84ab046b2
Guillaume Lacasa <guillaumelacasa@hotmail.com>
2017-05-07 17:25:13 +0200

    MediaTests moved to PostTests.UploadMedia

--- Mastonet.Tests/PostTests.cs
+++ Mastonet.Tests/PostTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -12,7 +13,24 @@ namespace Mastonet.Tests
         public async Task UploadMedia()
         {
             var client = GetTestClient();
-            throw new NotImplementedException();
+
+            System.IO.FileStream fs = new System.IO.FileStream(
+                                                @".\testimage.png",
+                                                System.IO.FileMode.Open,
+                                                System.IO.FileAccess.Read);
+            var attachment = await client.UploadMedia(fs, "testimage.png");
+            fs.Dispose();
+
+            Assert.NotNull(attachment);
+            Assert.NotNull(attachment.PreviewUrl);
+            Assert.NotNull(attachment.Url);
+
+            var status = await client.PostStatus("Status with imate", Visibility.Private, mediaIds: new int[] { attachment.Id });
+            status = await client.GetStatus(status.Id);
+
+            Assert.NotNull(status.MediaAttachments);
+            Assert.True(status.MediaAttachments.Any());
+            Assert.Equal(attachment.Url, status.MediaAttachments.First().Url);
         }
 
         [Fact]
