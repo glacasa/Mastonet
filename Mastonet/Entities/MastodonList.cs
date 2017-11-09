d3bd527eed97e7a97ad16d87399c03ffd628061d
glacasa <github@adhess.net>
2017-11-09 20:19:43 +0100

    MaxId and SinceId are nullables

--- Mastonet/Entities/MastodonList.cs
+++ Mastonet/Entities/MastodonList.cs
@@ -6,7 +6,7 @@ namespace Mastonet.Entities
 {
     public class MastodonList<T> : List<T>
     {
-        public long NextPageMaxId { get; internal set; }
-        public long PreviousPageSinceId { get; internal set; }
+        public long? NextPageMaxId { get; internal set; }
+        public long? PreviousPageSinceId { get; internal set; }
     }
 }
