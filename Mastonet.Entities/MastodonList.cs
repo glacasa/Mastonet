07c25f1f3b2629db502545875d9850366f15ea04
Guillaume Lacasa <github@adhess.net>
2023-05-08 18:23:45 +0200

    Separate entities in new project

---
+++ Mastonet.Entities/MastodonList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastonet.Entities;
+
+public class MastodonList<T> : List<T>
+{
+    public string? NextPageMaxId { get; set; }
+    public string? PreviousPageSinceId { get; set; }
+    public string? PreviousPageMinId { get; set; }
+}
