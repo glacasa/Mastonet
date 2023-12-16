1ff9615e81614eb486bb5ef781d3c7468ed283cc
Eduardo Braga <braga.eduardoTL@outlook.com>
2023-12-16 18:45:29 -0300

    Change history properties to string

--- Mastonet.Entities/History.cs
+++ Mastonet.Entities/History.cs
@@ -11,13 +11,13 @@ public class History
     /// UNIX timestamp on midnight of the given day.
     /// </summary>
     [JsonPropertyName("day")]
-    public int Day { get; set; }
+    public string Day { get; set; }
 
     /// <summary>
     /// the counted usage of the tag within that day.
     /// </summary>
     [JsonPropertyName("uses")]
-    public int Uses { get; set; }
+    public string Uses { get; set; }
 
     /// <summary>
     /// the total of accounts using the tag within that day.
